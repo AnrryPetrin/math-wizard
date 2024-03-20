57cc794529e36fc9665a5d6aec0da63a31b11b1e
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-03-19 21:46:27 -0300

    test: implement sum tests

--- src/main/java/com/anrry/CalculatorTest.java
+++ src/main/java/com/anrry/CalculatorTest.java
@@ -4,9 +4,26 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        double add;
+        double sum;
         double multiply;
         double divide;
         double subtract;
+        
+        // SUM
+        // test scenario 1:
+        sum = calculator.sum(3, 7);
+        System.out.println(sum);
+
+        // test scenario 2:
+        sum = calculator.sum(3, 0);
+        System.out.println(sum);
+
+        // test scenario 3:
+        sum = calculator.sum(0, 0);
+        System.out.println(sum);
+
+        // test scenario 4:
+        sum = calculator.sum(3, -1);
+        System.out.println(sum);
     }
 }
