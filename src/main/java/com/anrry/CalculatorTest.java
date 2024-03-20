b3d5a0569387dd015942fa52273cefedbb73e268
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-03-19 21:40:27 -0300

    test: initial test setup

--- src/main/java/com/anrry/CalculatorTest.java
+++ src/main/java/com/anrry/CalculatorTest.java
@@ -1,5 +1,12 @@
 package com.anrry;
 
 public class CalculatorTest {
-    
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        double add;
+        double multiply;
+        double divide;
+        double subtract;
+    }
 }
