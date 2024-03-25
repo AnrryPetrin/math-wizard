c0ea29372345f86eb940fb156dfb43d4b94a1733
Anrry Petrin De Araujo <108158140+AnrryPetrin@users.noreply.github.com>
2024-03-25 01:28:42 -0300

    fix: replace IllegalArgumentException to ArithmeticException when divide by zero

--- src/main/java/com/anrry/Calculator.java
+++ src/main/java/com/anrry/Calculator.java
@@ -15,7 +15,7 @@ public class Calculator {
 
     public double divide(double a, double b) {
         if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
+            throw new ArithmeticException("Cannot divide by zero");
         }
         return a / b;
     }
